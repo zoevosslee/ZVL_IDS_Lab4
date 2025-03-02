c15d1145cd6f8851df832ec83b3446fc7bfe2105
zoevosslee <zvl@mit.edu>
2025-03-02 12:17:09 -0500

    changed basepath

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ const config = {
 	kit: {
 		adapter: adapter({ fallback: '404.html' }),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/${process.env.BASE_PATH}',
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
 		},
 	},
 };
