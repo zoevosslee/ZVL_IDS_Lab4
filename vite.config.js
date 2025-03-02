1361b7f55d8ec929df71c03e1709e2dd61f92bef
zoevosslee <zvl@mit.edu>
2025-03-02 12:04:28 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+export default {
+	plugins: [sveltekit()],
+	base: process.env.BASE_PATH || "/ZVL_IDS_Lab4", // ✅ Ensures GitHub Pages gets the correct repo name
+  };
